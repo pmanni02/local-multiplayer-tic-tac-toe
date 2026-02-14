903af6fbb4b30e2d57581597c33a7a0b4842adca
= <=>
2026-02-13 21:47:39 -0800

    add events module and gateway

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // serve static SPA from client directory
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+    EventsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ apps/api/src/events/events.gateway.ts
@@ -0,0 +1,58 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+const clients: Record<string, Socket> = {};
+const numClients = (clientsObj: Record<string, Socket>) =>
+  Object.keys(clientsObj).length;
+
+@WebSocketGateway()
+export class EventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+
+  afterInit() {
+    console.log('Websocket server initialized!');
+  }
+
+  handleConnection(client: Socket) {
+    clients[client.id] = client;
+
+    console.log(
+      `Client: ${client.id} connected. Total: ${numClients(clients)}`,
+    );
+  }
+
+  handleDisconnect(client: Socket) {
+    for (const [key] of Object.entries(clients)) {
+      if (key === client.id) {
+        delete clients[key];
+      }
+    }
+
+    console.log(
+      `Client: ${client.id} disconnected. Total: ${numClients(clients)}`,
+    );
+  }
+
+  @SubscribeMessage('events')
+  handleEvent(
+    @MessageBody() data: string,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    console.log(`received: ${data}, broadcasting...`);
+    this.server.emit('events', {
+      message: data,
+      senderSocketId: client.id,
+    });
+  }
+}
