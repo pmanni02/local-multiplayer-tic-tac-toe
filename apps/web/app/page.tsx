18ac7c7c56e84159fbeef9b3c3bc2a222f42e321
= <=>
2026-02-14 12:26:14 -0800

    breakout Square component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,27 +1,7 @@
 'use client';
-import { JSX, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import io, { Socket } from 'socket.io-client';
-
-function Square({
-  value,
-  onClickFn
-}: {
-  value: 'X' | 'O',
-  onClickFn: () => void
-}): JSX.Element {
-  const textStyles = {
-    'X': 'text-red-500',
-    'O': 'text-blue-500'
-  }
-  return (
-    <div>
-      <button
-        className={`${textStyles[value]} font-bold p-0.5 w-25 h-25 bg-slate-500/50 hover:bg-slate-500/60 border border-slate-600/30 shadow-md`}
-        onClick={onClickFn}
-      >{value}</button>
-    </div>
-  )
-}
+import { Square } from "./square";
 
 export default function Board() {
   const [playerChar, setPlayerChar] = useState<'X' | 'O'>('X')
