608ab46d716eba3b31994d0e441abf3a75dbd981
= <=>
2026-02-13 18:27:14 -0800

    add tailwind config and tic-tac-toe board

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,102 +1,124 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+'use client';
+import { JSX, useState } from "react";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
+function Square({
+  value,
+  onClickFn
+}: {
+  value: 'X' | 'O',
+  onClickFn: () => void
+}): JSX.Element {
+  const textStyles = {
+    'X': 'text-red-500',
+    'O': 'text-blue-500'
+  }
+  return (
+    <div>
+      <button
+        className={`${textStyles[value]} font-bold p-0.5 w-25 h-25 bg-slate-500/50 hover:bg-slate-500/60 border border-slate-600/30 shadow-md`}
+        onClick={onClickFn}
+      >{value}</button>
+    </div>
+  )
+}
 
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
+export default function Board() {
+  const [playerChar, setPlayerChar] = useState<'X' | 'O'>('X')
+  const [squares, setSquares] = useState(Array(9).fill(''))
+  const [gameStatus, setGameStatus] = useState("")
+
+  const click = (index: number): void => {
+    if (squares[index] || gameStatus !== "") {
+      return
+    }
+
+    const squaresCopy: string[] = squares.slice()
+    squaresCopy[index] = playerChar;
+    setSquares(squaresCopy)
+
+    if (gameWon(squaresCopy)) {
+      setGameStatus('WINNER!')
+    } else if (gameTie(squaresCopy)) {
+      setGameStatus('TIE!')
+    }
+
+    if (playerChar === 'X') {
+      setPlayerChar('O')
+    } else {
+      setPlayerChar('X')
+    }
+  }
+
+  const gameTie = (squares: string[]) => {
+    const emptySquares = squares.filter(val => val === "")
+    if (emptySquares.length === 0) {
+      return true
+    }
+    return false
+  }
+
+  const gameWon = (squares: string[]) => {
+    // if values at 0,1,2 OR 3,4,5 OR 6,7,8 are all the same (rows)
+    // if values at 0,3,6 OR 1,4,7 OR 2,5,8 are all the same (cols)
+    // if values at 0,4,8 OR 2,4,6 are all the same (diagonals)
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+    let gameOver = false;
+    winConditions.forEach((winCondition) => {
+      const [x, y, z] = winCondition;
+      const validWinCondition =
+        squares[x!] !== "" &&
+        squares[x!] === squares[y!] &&
+        squares[y!] === squares[z!]
+      if (validWinCondition) {
+        gameOver = true;
+        return;
+      }
+    })
+    return gameOver
+  }
+
+  const resetSquares = () => {
+    const newSquares = Array(9).fill("")
+    setSquares(newSquares)
+    setGameStatus("")
+  }
 
   return (
     <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turborepo.dev/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className="p-40">
+        <h2 className="flex justify-center text-xl">Tic Tac Toe</h2>
+        <p className="flex justify-center text-black text-l font-bold h-8">{gameStatus}</p>
+        <div className="flex flex-col">
+          <div className="flex flex-row justify-center h-[103px] gap-[3px]">
+            <Square value={squares[0]} onClickFn={() => click(0)} />
+            <Square value={squares[1]} onClickFn={() => click(1)} />
+            <Square value={squares[2]} onClickFn={() => click(2)} />
+          </div>
+          <div className="flex flex-row justify-center h-[103px] gap-[3px]">
+            <Square value={squares[3]} onClickFn={() => click(3)} />
+            <Square value={squares[4]} onClickFn={() => click(4)} />
+            <Square value={squares[5]} onClickFn={() => click(5)} />
+          </div>
+          <div className="flex flex-row justify-center h-[103px] gap-[3px]">
+            <Square value={squares[6]} onClickFn={() => click(6)} />
+            <Square value={squares[7]} onClickFn={() => click(7)} />
+            <Square value={squares[8]} onClickFn={() => click(8)} />
+          </div>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://turborepo.dev?utm_source=create-turbo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to turborepo.dev →
-        </a>
-      </footer>
-    </div>
-  );
+        <div className="flex flex-row justify-center p-[2px]">
+          <button type="button" className="text-white bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded" onClick={() => resetSquares()}>Reset</button>
+        </div>
+      </div>
+    </>
+  )
 }
