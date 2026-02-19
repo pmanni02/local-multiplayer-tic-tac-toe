781e009dd6d4ee288b0ac45a3d248a4121a201b2
= <=>
2026-02-18 21:01:58 -0800

    update rooms dropdown and add room button styles

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 export default function Page() {
@@ -14,6 +14,8 @@ export default function Page() {
     <>
       <div>
         <h1 className="text-3xl">Tic Tac Toe</h1>
+
+        {/* GAME TYPE */}
         <details className="dropdown w-40">
           <summary className="flex flex-row btn-primary m-1 list-none bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded">
             Game Type
@@ -40,9 +42,10 @@ export default function Page() {
           </ul>
         </details>
 
+        {/* ROOM(S) */}
         <details className="dropdown w-40">
           <summary className="flex flex-row btn-primary m-1 list-none bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded">
-            Room
+            Room(s)
             <svg
               className="w-4 h-4 ms-1.5 -me-0.5"
               aria-hidden="true"
@@ -60,32 +63,41 @@ export default function Page() {
             </svg>
           </summary>
           <ul className="menu dropdown-content rounded-box z-1 p-2 shadow-sm">
-            <Link className="" href="/game">
-              {/* <li>Regular</li> */}
-            </Link>
+            {
+              rooms.map((room, index) => {
+                return <li key={index}>{room}</li>
+              })
+            }
           </ul>
         </details>
 
-        <div className="w-40">
-          <form onSubmit={(e) => {
-            e.preventDefault();
-            const target = e.target as typeof e.target & {
-              addRoom: { value: string }
-            };
-            const name = target.addRoom.value
-            const myRooms = [...rooms, name]
-            setRooms(myRooms)
-          }}>
+        <div className="flex flex-col w-40">
+          <form
+            className="flex flex-col"
+            id="roomInput"
+            onSubmit={(e) => {
+              e.preventDefault();
+              const target = e.target as typeof e.target & {
+                addRoom: { value: string }
+              };
+              const newRoom = target.addRoom.value;
+              console.log('newRoom', newRoom)
+
+              const roomsCopy = rooms.slice()
+              if (!roomsCopy.includes(newRoom)) {
+                const myRooms = [...rooms, newRoom]
+                setRooms(myRooms)
+              }
+
+              const form = document.getElementById('roomInput') as HTMLFormElement
+              form.reset()
+            }}>
             <input
               name="addRoom"
               className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
               placeholder="Add room name..."
             />
-            <button
-              type="submit"
-              className="text-white text-sm bg-green-500 py-2 px-4 rounded"
-            >Add Room
-            </button>
+            <p className="flex label text-xs justify-end-safe text-slate-500/50">Press Enter</p>
           </form>
 
         </div>
