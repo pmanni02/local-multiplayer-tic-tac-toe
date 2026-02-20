11b27c5a86ec54b436e2b34c5c7be70cef23e8ee
= <=>
2026-02-20 15:18:43 -0800

    update backend room/player char objects, join & only emit to own room

--- apps/api/src/events/events.gateway.ts
+++ apps/api/src/events/events.gateway.ts
@@ -15,10 +15,20 @@ type SocketInfo = {
   roomName: string;
 };
 
+type Game = {
+  numPlayers: number;
+  playerSocketIds: string[];
+  gameType: string;
+};
+
+let ROOM_ID = 1;
+const GAME_MAP: Map<string, Game> = new Map();
+
+// TODO: merge GAME_MAP and players
 const players: Record<string, SocketInfo> = {};
 const numPlayers = () => Object.keys(players).length;
 
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({ cors: { origin: '*' } })
 export class EventsGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
@@ -30,21 +40,50 @@ export class EventsGateway
 
   // On connection, determine room and player char
   handleConnection(socket: Socket) {
-    const tempDefaultRoom = 'room1';
+    const roomName = `room${ROOM_ID}`;
     if (
       numPlayers() === 0 ||
       (numPlayers() === 1 && Object.entries(players)[0][1].playerChar !== 'X')
     ) {
-      players[socket.id] = { playerChar: 'X', roomName: tempDefaultRoom };
+      players[socket.id] = { playerChar: 'X', roomName: roomName };
     } else {
-      players[socket.id] = { playerChar: 'O', roomName: tempDefaultRoom };
+      players[socket.id] = { playerChar: 'O', roomName: roomName };
     }
+
+    // check for existing game
+
+    const game = GAME_MAP.get(roomName);
+
+    // game exists but room is not full
+    if (game && game.numPlayers <= 1) {
+      const updatedGame = {
+        ...game,
+        numPlayers: game.numPlayers + 1,
+        playerSocketIds: [...game.playerSocketIds, socket.id],
+      };
+      GAME_MAP.set(roomName, updatedGame);
+      ROOM_ID += 1;
+    } else {
+      const newGame: Game = {
+        gameType: 'regular',
+        playerSocketIds: [socket.id],
+        numPlayers: 1,
+      };
+      GAME_MAP.set(roomName, newGame);
+    }
+
+    console.log('GAME_MAP', GAME_MAP);
+
+    // join room
+    void socket.join(roomName);
+
     console.log(
       `[CONNECTED]: ${socket.id}, char: ${players[socket.id].playerChar}, room: ${players[socket.id].roomName}`,
     );
   }
 
   handleDisconnect(socket: Socket) {
+    // TODO: update to handle new GAME_MAP
     for (const [key] of Object.entries(players)) {
       if (key === socket.id) {
         delete players[key];
@@ -79,7 +118,7 @@ export class EventsGateway
     @ConnectedSocket() socket: Socket,
   ): void {
     console.log(`[EVENTS]: ${JSON.stringify(data)} for ROOM: ${data.room}`);
-    this.server.emit('events', {
+    this.server.to(data.room).emit('events', {
       squares: data.squares,
       status: data.status,
       currentPlayer: data.currentPlayer,
