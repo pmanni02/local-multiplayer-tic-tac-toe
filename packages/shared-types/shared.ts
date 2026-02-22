85e2e325f8ae1e2db14c9163edea2ba808942d87
= <=>
2026-02-21 22:19:33 -0800

    add shared-types package, update package lock

---
+++ packages/shared-types/shared.ts
@@ -0,0 +1,28 @@
+export type Nullable<T> = T | null | undefined;
+
+export type GameStatusMessage = {
+  message: string;
+  status: string;
+};
+
+export type RoomDeterminedMessage = {
+  roomName: string;
+};
+
+export type GameInitializedMessage = {
+  roomName: string;
+};
+
+export type EventsMessageToServer = {
+  squares: string[];
+  status: string;
+  currentPlayer: string;
+  room: string;
+};
+
+export type EventsMessageToClient = Omit<EventsMessageToServer, 'room'>
+
+export type GAME_CONNECTION_STATES =
+  | "connected"
+  | "disconnected"
+  | "pendingGame";
